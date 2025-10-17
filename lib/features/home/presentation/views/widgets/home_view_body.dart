8fbafcd3d8be9516742aad5de5b95f7760590d75
Ehab_Ahmed143 <ehabahmedabdrabo@gmail.com>
2025-10-17 14:30:45 +0300

    create styles class &add title to home view

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,14 +1,25 @@
+import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
 import 'custom_appBar.dart';
 import 'featured_books_list_view.dart';
-import 'featured_list_view_item.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [CustomAppBar(), FeaturedBooksListView()]);
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomAppBar(),
+          FeaturedBooksListView(),
+          SizedBox(height: 50),
+          Text('Best Seller', style: Styles.titleMedium),
+        ],
+      ),
+    );
   }
 }
