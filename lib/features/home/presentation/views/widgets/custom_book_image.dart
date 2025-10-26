bf9d926da4d3fe62d705487b58da3407a5e06779
Ehab_Ahmed143 <ehabahmedabdrabo@gmail.com>
2025-10-26 14:54:15 +0300

    cached network image

--- lib/features/home/presentation/views/widgets/custom_book_image.dart
+++ lib/features/home/presentation/views/widgets/custom_book_image.dart
@@ -1,22 +1,35 @@
 import 'package:bookly_app/core/utils/assets.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class CustomBookImage extends StatelessWidget {
   const CustomBookImage({super.key, required this.imageUrl});
   final String imageUrl;
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 2.6 / 4,
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
-          image: DecorationImage(
-            fit: BoxFit.fill,
-            image: NetworkImage(imageUrl),
-          ),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      child: AspectRatio(
+        aspectRatio: 2.6 / 4,
+        child: CachedNetworkImage(
+          fit: BoxFit.fill,
+          imageUrl: imageUrl,
+          errorWidget: (context, url, error) => Icon(Icons.error),
         ),
       ),
     );
   }
 }
+
+
+
+// Container(
+//         decoration: BoxDecoration(
+//           borderRadius: BorderRadius.circular(16),
+//           image: DecorationImage(
+//             fit: BoxFit.fill,
+//             image: NetworkImage(imageUrl),
+//           ),
+//         ),
+//       ),
